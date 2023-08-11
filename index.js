c6585e9e5a4a772c610f5d6b128cdf74d8b96a7f
GastonPetiti <sgastonpetiti@gmail.com>
2023-08-11 00:46:34 -0300

    sacando codigo innecesario

--- index.js
+++ index.js
@@ -46,81 +46,73 @@ const pizzas = [
   },
 ];
 
-const container = document.querySelector('#container')
-const form = document.getElementById('form')
-const inputNumber = document.querySelector('#number-input')
-const btnSubmit = document.querySelector('#btn-submit')
-const pizzaContainer = document.querySelector('#pizza-container')
-const error = document.getElementById('error')
-
+const container = document.querySelector("#container");
+const form = document.getElementById("form");
+const inputNumber = document.querySelector("#number-input");
+const btnSubmit = document.querySelector("#btn-submit");
+const pizzaContainer = document.querySelector("#pizza-container");
+const error = document.getElementById("error");
 
 //funcion para buscar la pizza con el id ingresado y guardar en localStorage la pizza
-const buscarPizza = (e)=>{
-  e.preventDefault()
-  const pizza = pizzas.find((pizza) => pizza.id == inputNumber.value)
 
-  if(inputNumber.value == ""){
-    pizzaContainer.innerHTML = ""
-    error.style.display = "flex"
-    const textError = error.textContent = `No se ingreso ningún número. Tenes que ingresar un número`
+const buscarPizza = (e) => {
+  e.preventDefault();
+  const pizza = pizzas.find((pizza) => pizza.id == inputNumber.value);
 
-    const sessionError = sessionStorage.setItem('error',JSON.stringify(textError))
-    sessionStorage.getItem('error',JSON.parse(sessionStorage.getItem(sessionError)))
-    
-    return    
+  if (inputNumber.value == "") {
+    pizzaContainer.innerHTML = "";
+    error.style.display = "flex";
+    const textError =
+      (error.textContent = `No se ingreso ningún número. Tenes que ingresar un número`);
+    return;
   }
 
-  
-  if(pizza){
-   const cardPizza = pizzaContainer.innerHTML=`<div id="card-pizza">
+  if (pizza) {
+    const cardPizza = (pizzaContainer.innerHTML = `<div id="card-pizza">
     <h2>${pizza.nombre}</h2>
     <img src=${pizza.imagen} alt="${pizza.name}" class="img-container"/>
     <p>$${pizza.precio}</p>
-    </div> `
-    inputNumber.value=""
-    error.style.display ="none"
+    </div> `);
+    inputNumber.value = "";
+    error.style.display = "none";
 
-    const localPizza = localStorage.setItem("pizza", JSON.stringify(pizza))
+    const localPizza = localStorage.setItem("pizza", JSON.stringify(pizza));
   }
-  
-  if(!pizza){
-    pizzaContainer.innerHTML = ""
-    error.style.display = "flex"
-    error.classList.add('alert-msj')
-    const textError = error.textContent = `No hay una pizza con el número ingresado`
-    inputNumber.value=""
 
-    const sessionError = sessionStorage.setItem('error',JSON.stringify(textError))
-    JSON.parse(sessionStorage.getItem(sessionError))
-
-    return
+  if (!pizza) {
+    pizzaContainer.innerHTML = "";
+    error.style.display = "flex";
+    error.classList.add("alert-msj");
+    const textError =
+      (error.textContent = `No hay una pizza con el número ingresado`);
+    inputNumber.value = "";
+    return;
   }
-}
+};
 
 //funcion para traer la pizza del localStorage
 
 const pizzaStorage = () => {
-  const ultimaPizza = JSON.parse(localStorage.getItem("pizza"))
+  const ultimaPizza = JSON.parse(localStorage.getItem("pizza"));
 
-  if(ultimaPizza){
-    pizzaContainer.innerHTML=`<div id="card-pizza">
+  if (ultimaPizza) {
+    pizzaContainer.innerHTML = `<div id="card-pizza">
     <h2>${ultimaPizza.nombre}</h2>
     <img src=${ultimaPizza.imagen} alt="${ultimaPizza.name}" class="img-container"/>
     <p>$${ultimaPizza.precio}</p>
-    </div> `
-    inputNumber.value=""
-    error.style.display ="none"
-  } else{
-    pizzaContainer.innerHTML = ""
+    </div> `;
+    inputNumber.value = "";
+    error.style.display = "none";
+  } else {
+    pizzaContainer.innerHTML = "";
   }
-}
-
-
+};
 
 //funcion inicializadora
-function init () {
-  document.addEventListener("DOMContentLoaded",pizzaStorage)
-  form.addEventListener('submit', buscarPizza)
+
+function init() {
+  document.addEventListener("DOMContentLoaded", pizzaStorage);
+  form.addEventListener("submit", buscarPizza);
 }
 
-init()
+init();
